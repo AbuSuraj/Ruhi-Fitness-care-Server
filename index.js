ede2532a6cedf0b71a75883cf7a422faf3f1afec
abu bokor suraj <abusurajbd@gmail.com>
2022-11-07 23:03:59 +0600

    database added with server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,6 +8,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.sc93kvm.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 app.get('/', (req, res) => {
     res.send('Ruhi fitness care server is running')
